3a0c82efdd130f3b76a06d3d582b0a9751f3ad0b
Brad Collett <bcollett@me.com>
2020-10-23 20:32:30 -0500

    update

--- LogosLibrary/settings.py
+++ LogosLibrary/settings.py
@@ -119,11 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = ''
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    '/var/www/static/',
-]
+STATIC_ROOT = '/home/logoslib/logoslib.pythonanywhere.com/static/'
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
